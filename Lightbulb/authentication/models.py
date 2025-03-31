8dd2304ac9d86b77e1ea9e41d11f7c34173dae2f
darragh50 <darraghrohan@gmail.com>
2025-03-31 21:43:50 +0100

    Create django post model & import UUID, datetime

--- Lightbulb/authentication/models.py
+++ Lightbulb/authentication/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
+from datetime import datetime
 # Create your models here.
 
 class Profile(models.Model):
@@ -11,3 +13,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+# Post model - contains unique_id, image, caption, time created, likes
+# Return username of user who posted 
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    numLikes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
